3a8843918449281c8bd4b09262134f519eb4b217
zhangsan12121 <340406548@qq.com>
2020-09-30 14:57:17 +0800

    修改test--bbbbb

--- src/main/java/com/wangjin/Test.java
+++ src/main/java/com/wangjin/Test.java
@@ -7,5 +7,6 @@ package com.wangjin;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbb");
     }
 }
